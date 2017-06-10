e439dcd4a7ed91f6a3465ed2cb23b573d4ab1c28
Daniel Lewis <dan.lewis@enspiral.com>
2017-06-10 19:15:28 +1200

    add type gen command

--- bin/commands.js
+++ bin/commands.js
@@ -1,17 +1,35 @@
 #! /usr/bin/env node
-var yargs = require("yargs");
-var shell = require("shelljs");
+var yargs = require("yargs")
+var shell = require("shelljs")
+var fs = require('fs')
+var R = require('ramda')
+var compileFromFile = require('json-schema-to-typescript').compileFromFile
 
 console.log("Breed typescript interfaces from jsonschema")
 
 var argv = yargs.usage("$0 <cmd> [args]")
-  .command("jsonschema [name]", "create a new jsonschema of [name]", {
+  .command("schema [name]", "create a new jsonschema of [name]", {
     name: {
       default: 'example'
     }
   }, function (yargs) {
-    shell.exec(`mkdir ${yargs.name} && touch ${yargs.name}/${yargs.name}.json`);
+    shell.exec(`mkdir -p json-schemas/${yargs.name} && touch json-schemas/${yargs.name}/${yargs.name}.json`)
   })
+  .command("type [name]", "create types based on [name]", {
+    name: {
+      default: 'example'
+    }
+  }, function (yargs) {
+    shell.exec(`mkdir -p ${yargs.name}/types`)
+    fs.readdir(`json-schemas/${yargs.name}`, function (err, files) {
+      if (err) return err
+      console.log(files)
+      R.map(function (file) {
+        compileFromFile(`json-schemas/${yargs.name}/${file}`)
+        .then(ts => fs.writeFileSync(`${yargs.name}/types/${file}.ts`, ts))
+      }, files)
+    })
+ })
   .demand(1, "must provide a valid command")
   .help("h")
   .alias("h", "help")
